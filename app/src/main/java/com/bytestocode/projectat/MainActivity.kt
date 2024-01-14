65c99b9e98c8b285eec3987531b22f50a15c3224
Jaecheol Park <nature9th@gmail.com>
2024-01-14 13:12:11 +0900

    feat: text composables
    - android basics with compose Unit 1 Pathway 3

--- app/src/main/java/com/bytestocode/projectat/MainActivity.kt
+++ app/src/main/java/com/bytestocode/projectat/MainActivity.kt
@@ -3,29 +3,35 @@ package com.bytestocode.projectat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.bytestocode.projectat.ui.theme.ProjectATTheme
+import androidx.compose.ui.unit.sp
+import com.bytestocode.projectat.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ProjectATTheme {
+            HappyBirthdayTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Jaecheol Park")
+                    GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
                 }
             }
         }
@@ -33,19 +39,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
         Text(
-            text = "Hi, my name is $name!",
-            modifier = modifier.padding(24.dp)
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End),
         )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ProjectATTheme {
-        Greeting("Jaecheol Park")
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        GreetingText(
+            message = "Happy Birthday Sam!",
+            from = "From: John Doe",
+            modifier = Modifier.padding(16.dp),
+        )
     }
 }
