7eb8f9d7285ba1b8c3a22abb187fad3a7bffc0bf
Jaecheol Park <nature9th@gmail.com>
2024-01-21 13:03:07 +0900

    feat: Add a scrollable list
    - Course: android basics with compose
    - Unit 3: Display lists and use Material Design
    - Pathway 2: Build a scrollable list

--- app/src/androidTest/java/com/bytestocode/projectat/TipUITests.kt
+++ app/src/androidTest/java/com/bytestocode/projectat/TipUITests.kt
@@ -1,13 +1,5 @@
 package com.bytestocode.projectat
 
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performTextInput
-import com.bytestocode.projectat.ui.theme.CustomTheme
-import org.junit.Rule
-import org.junit.Test
-import java.text.NumberFormat
-
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -15,22 +7,4 @@ import java.text.NumberFormat
  */
 
 class TipUITests {
-    @get:Rule
-    val composeTestRule = createComposeRule()
-
-    @Test
-    fun calculate_20_percent_tip() {
-        composeTestRule.setContent {
-            CustomTheme {
-                TipTimeLayout()
-            }
-        }
-        composeTestRule.onNodeWithText("Bill Amount")
-            .performTextInput("10")
-        composeTestRule.onNodeWithText("Tip Percentage").performTextInput("20")
-        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
-        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
-            "No node with this text was found."
-        )
-    }
 }
---
+++ app/src/main/java/com/bytestocode/projectat/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.bytestocode.projectat.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
